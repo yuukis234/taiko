b25a3c0bbc79db763da081730a0e6c5678cfd7dc
yuuki <kumagai.robo@gmail.com>
2019-06-24 06:49:36 +0900

    セーブ！

--- src/main/kotlin/udp/udp_socket.kt
+++ src/main/kotlin/udp/udp_socket.kt
@@ -13,7 +13,9 @@ class UDP()
           val data = ByteArray(1024)
           var packet = DatagramPacket(data, data.size)
           server.receive(packet)
-          val output = String(data, Charsets.UTF_8)
+          val test = "omega drive"
+          val test_byte = test.toByteArray(Charsets.UTF_8)
+          packet.setData(test_byte)
           server.send(packet)
         } catch (e: Exception) {
           println("error")
